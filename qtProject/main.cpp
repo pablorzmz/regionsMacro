6a7a29680c34bec6a0b5d48feb156182fb9f9136
Jose Pablo <pablow75@hotmail.com>
2018-07-22 19:28:24 -0600

    version 1 regiones c++

---
+++ qtProject/main.cpp
@@ -0,0 +1,232 @@
+#include <iostream>
+#include "region.h"
+
+using namespace std;
+
+int chancho = 500;
+int nH = 0;
+int nO = 0;
+int fH = 0;
+int fO = 0;
+
+//#define CHANCHO
+#define regionR
+#define AGUA
+#define regionS
+
+#ifdef CHANCHO
+    INIT_REGION( regionR,2,1 )
+#endif
+
+#ifdef AGUA
+    INIT_REGION( regionS,4,4 )
+#endif
+
+
+#define numHilos 100
+sem_t consola;
+pthread_t hiloNuevo[numHilos];
+void printControlled( std::string msg );
+int status = 0;
+
+/*
+    Espera por la ejecución de todos los demás hilos.
+    Consecuentemente, si el hilo se atrapa en la región, la ejecución no termina nunca.
+*/
+void esperarHilos()
+{
+    for( int x = 0; x < numHilos; ++x )
+    {
+        pthread_join( hiloNuevo[x], (void**)& status );
+    }
+}
+
+#ifdef CHANCHO
+    void* meterAlChancho( void* cantidad )
+    {
+        REGION ( regionR,0,chancho )
+
+        DO ( regionR, 0,
+               long dummy = (long)cantidad;
+               std::string message = "METER AL CHANCHO $";
+               message += to_string( dummy );
+               message += "\n\tEl chancho actualmente tiene ";
+               message += to_string( chancho );
+               message += "\n\tChancho ahora tiene ";
+               message += to_string( chancho + dummy );
+               message += "\n\n";
+               printControlled( message );
+               chancho+=( dummy );
+           )
+
+        return NULL;
+    }
+#endif
+
+#ifdef CHANCHO
+    void* sacarDelChancho(void* cantidad)
+    {
+        long dummy = (long)cantidad;
+        cout<<"SACAR DEL CHANCHO( $"<<chancho<<" ): $"<<dummy<<" Se va a poder ? "<<(chancho>=dummy?"true":"false")<<endl;
+
+        REGION( regionR,1,chancho ) WHEN ( regionR,1,chancho>=dummy )
+        DO_NC ( regionR,1,
+                   std::string message = "\tEl chancho actualmente tiene ";
+                   message +=  to_string(chancho);
+                   message += "\n\tUn hilo logra sacar ";
+                   message += to_string(dummy);
+                   message += " del chancho\n\n";
+                   printControlled(message);
+                   chancho-=(dummy);
+               )
+
+        return NULL;
+    }
+#endif
+
+
+#ifdef AGUA
+    void* O(void* id)
+    {
+        cout<<("O:: Oxigenos= " + to_string(nO) + " Hidrógenos= " + to_string(nH) + "\n");
+        REGION ( regionS,0,nO )
+        DO ( regionS,0,
+            nO++;
+           )
+
+        REGION ( regionS,1,nH,nO,fH,fO ) WHEN ( regionS,1, (fO > 0) || (nO > 0 && nH > 1) )
+        DO(regionS,1,
+           if ( fO > 0)
+              {
+                fO--;
+              }
+           else
+              {
+               std::string msg = "\tOxigenos= " + to_string(nO) + " Hidrógenos= " + to_string(nH) + "\n";
+               msg += "\tSoy el oxígeno n°: ";
+               msg += to_string((long)id);
+               msg += " y estoy \n";
+               msg +="\t\tHaciendo agua...\n\n";
+               printControlled(msg);
+               nO--;
+               nH -= 2;
+               fH = 2;
+              }
+           )
+        return NULL;
+    }
+
+    void* H( void* id )
+    {
+        cout<<("H:: Oxigenos= " + to_string(nO) + " Hidrógenos= " + to_string(nH) + "\n");
+        REGION ( regionS,2,nH )
+        DO ( regionS,2,
+            nH++;
+           )
+
+        REGION ( regionS,3,nH,nO,fH,fO) WHEN (regionS,3, (fH > 0) || (nO > 0 && nH > 1) )
+        DO ( regionS,3,
+             if( fH > 0 )
+               {
+                 fH--;
+               }
+               else
+               {
+                 std::string msg = "\tOxigenos= " + to_string(nO) + " Hidrógenos= " + to_string(nH) + "\n";
+                 msg += "\tSoy el hidrógeno n°: ";
+                 msg += to_string((long)id);
+                 msg += " y estoy \n";
+                 msg +="\t\tHaciendo agua...\n\n";
+                 printControlled(msg);
+                 nO--;
+                 nH -= 2;
+                 fH = 1;
+                 fO = 1;
+               }
+           )
+        return NULL;
+    }
+
+#endif
+
+/*
+    @Param msg: Mensaje a mostrar de manera controlada por la consola
+*/
+
+void printControlled( std::string msg )
+{
+    sem_wait( &consola );
+    cout<<msg;
+    sem_post( &consola );
+}
+
+
+int main()
+{
+
+sem_init(&consola,0,1);
+pthread_attr_t at;
+pthread_attr_init(&at);
+pthread_attr_setdetachstate( &at,PTHREAD_CREATE_JOINABLE );
+//long status = 0;
+
+#ifdef CHANCHO
+
+    SHARED( regionR,chancho )
+
+    long cantidadPedir = 0;
+    long cantidadMeter = 0;
+
+    for( int x = 0; x < numHilos; ++x )
+    {
+        cantidadPedir = 200 + rand()%1000;
+        cantidadMeter = 200 + rand()%900;
+
+        if (x%2 == 0)
+        {
+            pthread_create(&hiloNuevo[x],&at,sacarDelChancho,(void*)cantidadPedir);
+        }else
+        {
+            pthread_create(&hiloNuevo[x],&at,meterAlChancho,(void*)cantidadMeter);
+        }
+
+    }    
+    pthread_attr_destroy( &at );
+    //esperarHilos();
+#endif
+
+#ifdef AGUA
+
+    SHARED(regionS,nH)
+    SHARED(regionS,nO)
+    SHARED(regionS,fH)
+    SHARED(regionS,fO)
+
+    for ( long id = 0; id < numHilos; ++ id )
+    {
+        if ( id%2 == 0  )
+        {
+           pthread_create(&hiloNuevo[id],&at,H,(void*)(id+1));
+        }else
+        {
+           pthread_create(&hiloNuevo[id],&at,O,(void*)(id+1));
+        }
+    }
+
+    pthread_attr_destroy( &at );
+    //esperarHilos();
+
+#endif
+
+printControlled("Ejecución correcta\n");
+#ifdef CHANCHO
+    DESTROY_REGION( regionR )
+#endif
+
+#ifdef AGUA
+    DESTROY_REGION( regionS )
+#endif
+
+    return 0;
+}
+
---
+++ qtProject/region.h
@@ -0,0 +1,156 @@
+#ifndef REGION_H
+#define REGION_H
+
+#include <pthread.h>
+#include <vector>
+#include <iostream>
+#include <cstring>
+#include <semaphore.h>
+
+
+/*
+    Estructura para esta versión de la región que facilita el control de las variables a
+    compartir y los avisos para cuando ya se puede acceder a la región.
+*/
+struct sharedValues
+{
+    public:
+    std::string name;    
+    sem_t sem;
+    sharedValues()
+        :name("")        
+        ,sem()
+    {
+
+    }
+};
+
+/*
+    @Summary: Macro DESTROY_REGION para destruir todos los recursos creados al usar las regiones
+    @Param regionName: Nombre de la región, debe ser texto. Preferiblemente una definición de código.
+ */
+
+#define DESTROY_REGION( regionName ) \
+    for ( size_t index = 0; index < maxR##_##regionName; ++ index )\
+        {\
+            pthread_mutex_destroy( &maxSpacesForRegion##_##regionName[ index ] );\
+        };\
+    for ( size_t index2 = 0; index2 < maxV##_##regionName; ++ index2 )\
+        {\
+            sem_destroy( &semaphores##_##regionName[ index2 ] );\
+        };\
+
+
+/*
+    @Summary: Macro DESTROY_REGION para destruir todos los recursos creados al usar las regiones
+    @Param regionName: Nombre de la región, debe ser texto. Preferiblemente una definición de código.
+    @Param maxRegionCalls: Numero máximo de espacios de región para la región actualmente a definir
+    @Param maxSharedVariables: Número máximo de variables que se compartir dentro de la región
+*/
+
+#define INIT_REGION( regionName, maxRegionCalls, maxSharedVariables )\
+    pthread_mutex_t maxSpacesForRegion##_##regionName[ maxRegionCalls ];\
+    sem_t semaphores##_##regionName[ maxSharedVariables ];\
+    void initialize##_##regionName##_##aaabbbccc()\
+    {\
+        for (int x = 0; x < maxRegionCalls; ++x ) \
+        { \
+            pthread_mutex_init(&maxSpacesForRegion##_##regionName[ x ],NULL); \
+        } \
+    } \
+    sharedValues myValues##_##regionName[ maxSharedVariables ]; \
+    static size_t maxV##_##regionName = maxSharedVariables; \
+    static size_t maxR##_##regionName = maxRegionCalls; \
+    static size_t indexVaribales##_##regionName = 0; \
+    static bool  controlInit##_##regionName = false; \
+
+/*
+    @Summary: Macro SHARED para "compartir" una variable global en una región específica.
+    @Param regionName: Nombre de la región, debe ser texto. Preferiblemente una definición de código.
+    @Param variableName: Nombre del identificador de la variable para declarar como compartida en la región
+*/
+#define SHARED( regionName, variableName ) \
+    if ( controlInit##_##regionName == false ) \
+    { \
+        initialize##_##regionName##_##aaabbbccc();\
+        controlInit##_##regionName = true;\
+    } \
+    sharedValues temp##_##variableName;\
+    sem_t variableName##_##regionName;\
+    sem_init( &variableName##_##regionName,0,0 );\
+    semaphores##_##regionName[ indexVaribales##_##regionName ] = variableName##_##regionName;\
+    temp##_##variableName.sem = variableName##_##regionName;\
+    temp##_##variableName.name = #variableName;\
+    myValues##_##regionName[ indexVaribales##_##regionName ] = temp##_##variableName;\
+    ++indexVaribales##_##regionName;
+
+/*
+    @Summary: Macro REGION que declara un espacio a usar después de la incialización de la región como tal.
+    @Param regionName: Nombre de la región, debe ser texto. Preferiblemente una definición de código.
+    @Param spaceNumber: Número del espacio de la región a utilizar.
+    @Param ...: Lista de nombres de las variables definidas previamente con la macro SHARED como compar-
+    tidas que puedan o no cambiar dentro de la región.
+
+*/
+#define REGION( regionName,spaceNumber,... )\
+    static char constChar##_##regionName##_##spaceNumber[] = {#__VA_ARGS__};\
+    static char* p##_##regionName##_##spaceNumber = strtok(constChar##_##regionName##_##spaceNumber,",");\
+    static std::vector<std::string> vector##_##regionName##_##spaceNumber;\
+    while(p##_##regionName##_##spaceNumber)\
+    {\
+        vector##_##regionName##_##spaceNumber.push_back(p##_##regionName##_##spaceNumber);\
+        p##_##regionName##_##spaceNumber = strtok(NULL,",");\
+    };\
+    p##_##regionName##_##spaceNumber = NULL;\
+    pthread_mutex_lock( &maxSpacesForRegion##_##regionName[ spaceNumber ] );\
+
+/*
+    @Summary: Macro WHEN que detiene el acceso al región crítica.
+    @Param regionName: Nombre de la región, debe ser texto. Preferiblemente una definición de código.
+    @Param spaceNumber: Número del espacio de la región a utilizar.
+    @Param condition: Método o condición booleana para entrar a ejecutarse en la región.
+*/
+#define WHEN( regionName, spaceNumber, condition )\
+    while ( (condition) == false ){\
+        for (size_t x = 0; x <maxV##_##regionName; ++ x ){\
+            for ( size_t y = 0; y < vector##_##regionName##_##spaceNumber.size(); ++y ){\
+                    if ( vector##_##regionName##_##spaceNumber[y] == myValues##_##regionName[x].name )\
+                        {\
+                            sem_wait(&(myValues##_##regionName[x].sem));\
+                            y = vector##_##regionName##_##spaceNumber.size();\
+                            x = maxV##_##regionName;\
+                        }\
+                }\
+          }\
+    };\
+
+/*
+    @Summary: Macro DO para realizar lo que se desee cuando se entre a la región.
+    @Param regionName: Nombre de la region, debe ser texto. Preferiblemente una definición de código
+    @Param spaceNumber: Número del espacio de la región a utilizar.
+    @Ṕaram Code: Literalmente el código a ejecutar en la región.
+*/
+#define DO(regionName,spaceNumber,Code)\
+    Code\
+    for (size_t x = 0; x <maxV##_##regionName; ++ x ){\
+        for ( size_t y = 0; y < vector##_##regionName##_##spaceNumber.size(); ++y ){\
+                if ( vector##_##regionName##_##spaceNumber[y] == myValues##_##regionName[x].name  )\
+                    { \
+                        sem_post(&(myValues##_##regionName[x].sem));\
+                    }\
+            }\
+       }; \
+    pthread_mutex_unlock( &maxSpacesForRegion##_##regionName[ spaceNumber ] );\
+
+/*
+    @Summary: Similar al macro DO, pero se utiliza cuando no se va avisar a los hilos que esperan en la región.
+    @Param regionName: Nombre de la region, debe ser texto. Preferiblemente una definición de código
+    @Param spaceNumber: Número del espacio de la región a utilizar.
+    @Ṕaram Code: Literalmente el código a ejecutar en la región.
+*/
+#define DO_NC(regionName,spaceNumber,Code)\
+    Code \
+    pthread_mutex_unlock( &maxSpacesForRegion##_##regionName[ spaceNumber ] );\
+
+
+#endif // REGION_H
